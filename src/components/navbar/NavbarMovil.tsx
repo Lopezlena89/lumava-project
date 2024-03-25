364e43b0884e90537e841fb18035a8bf7a857bba
Luis Mariano Lopez Lena Valdivieso <luismllv89@gmail.com>
2024-03-24 20:21:15 -0600

    movil whatsapp

--- src/components/navbar/NavbarMovil.tsx
+++ src/components/navbar/NavbarMovil.tsx
@@ -3,6 +3,7 @@
 import Link from "next/link";
 import { usePathname } from "next/navigation";
 import { CiBrightnessDown,CiDark,CiViewList,CiGrid42,CiHome,CiMail,CiDesktop,CiPen  } from "react-icons/ci";
+import { PiWhatsappLogoThin } from "react-icons/pi";
 
 
 export const NavbarMovil = () => {
@@ -19,14 +20,14 @@ export const NavbarMovil = () => {
           <Link href="/web-developmen">
             <CiDesktop size={25} className={` ${path === "/web-developmen"?"text-violet-500":"text-zinc-200"}`}/>
           </Link>
-          <Link href="/design-and-branding">
+          {/* <Link href="/design-and-branding">
             <CiPen size={25} className={` ${path === "/design-and-branding"?"text-violet-500":"text-zinc-200"}`}/>
-          </Link>
+          </Link> */}
           <Link href="/offers">
             <CiViewList size={25} className={` ${path === "/offers"?"text-violet-500":"text-zinc-200"}`}/>
           </Link>
-          <Link href={"mailto:lumavaagency@gmail.com"}>
-            <CiMail size={25} className={`text-zinc-200 hover:text-violet-500`}/>
+          <Link href="https://wa.me/523315753378" >
+            <PiWhatsappLogoThin size={25} className={`text-zinc-200 hover:text-violet-500`}/>
           </Link>
           
       </div>
